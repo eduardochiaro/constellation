346ddea751a3adee6e199be71b0baea9d5f56840
Eduardo Chiaro <e.chiaro@gmail.com>
2025-12-31 12:15:30 +0100

    Add SysDef splash logo support

--- src/c/modules/splash_logo_module.c
+++ src/c/modules/splash_logo_module.c
@@ -6,6 +6,7 @@ static GBitmap *s_cbw_logo_bitmap;
 static GBitmap *s_ccolor_logo_bitmap;
 static GBitmap *s_housevarun_logo_bitmap;
 static GBitmap *s_freestar_logo_bitmap;
+static GBitmap *s_sysdef_logo_bitmap;
 
 void splash_logo_init(void) {
 
@@ -13,6 +14,7 @@ void splash_logo_init(void) {
   s_ccolor_logo_bitmap = gbitmap_create_with_resource(RESOURCE_ID_CONSTELLATION_COLOR_LOGO_IMAGE);
   s_housevarun_logo_bitmap = gbitmap_create_with_resource(RESOURCE_ID_HOUSEVARUUN_LOGO_IMAGE);
   s_freestar_logo_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FREESTAR_LOGO_IMAGE);
+  s_sysdef_logo_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SYSDEF_LOGO_IMAGE);
 }
 
 void splash_logo_show(Window *window, char *s_style_logo) {
@@ -29,6 +31,8 @@ void splash_logo_show(Window *window, char *s_style_logo) {
       splash_logo = s_housevarun_logo_bitmap;
     } else if (strcmp(s_style_logo, "freestar") == 0) {
       splash_logo = s_freestar_logo_bitmap;
+    } else if (strcmp(s_style_logo, "sysdef") == 0) {
+      splash_logo = s_sysdef_logo_bitmap;
     }
     
     if (splash_logo) {
@@ -67,5 +71,9 @@ void splash_logo_cleanup(void) {
   if (s_freestar_logo_bitmap) {
     gbitmap_destroy(s_freestar_logo_bitmap);
     s_freestar_logo_bitmap = NULL;
+  }
+  if (s_sysdef_logo_bitmap) {
+    gbitmap_destroy(s_sysdef_logo_bitmap);
+    s_sysdef_logo_bitmap = NULL;
   }
 }
