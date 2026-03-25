bcf375b90fd7079c72fc01a487da6f68ee6f8c5c
Eduardo Chiaro <e.chiaro@gmail.com>
2026-03-25 05:45:29 +0100

    Move shared resources to shared/ and update setup

--- setup.sh
+++ setup.sh
@@ -2,17 +2,23 @@
 
 # Define source and target directories
 
-DIRECTORY_TO_LINK="utilities"
+DIRECTORIES_TO_LINK=("src/c/utilities" "resources/weather" "resources/splash_logos")
 
-SOURCE_DIR="./shared/c/$DIRECTORY_TO_LINK"
-TARGET_DIR_LIST=("./chronomark-edition/src/c" "./standard-edition/src/c")
+TARGET_DIR_LIST=("./chronomark-edition/" "./standard-edition/")
 
-# Create target directories if they don't exist
-for TARGET_DIR in "${TARGET_DIR_LIST[@]}"; do
-  mkdir -p "$TARGET_DIR"
 
-  # Create the symlink inside the target directory
-  ln -sfn "$(realpath "$SOURCE_DIR")" "$TARGET_DIR/$DIRECTORY_TO_LINK"
+for DIRECTORY_TO_LINK in "${DIRECTORIES_TO_LINK[@]}"; do
+  SOURCE_DIR="./shared/$DIRECTORY_TO_LINK"
 
-  echo "Symlink created: $TARGET_DIR/$DIRECTORY_TO_LINK -> $(realpath "$SOURCE_DIR")"
+  if [ ! -d "$SOURCE_DIR" ]; then
+    echo "Error: Source directory '$SOURCE_DIR' does not exist."
+    exit 1
+  fi
+
+  # Create target directories and symlinks for each target directory
+  for TARGET_DIR in "${TARGET_DIR_LIST[@]}"; do
+    mkdir -p "$TARGET_DIR"
+    ln -sfn "$SOURCE_DIR" "$TARGET_DIR/$DIRECTORY_TO_LINK"
+    echo "Symlink created: $TARGET_DIR/$DIRECTORY_TO_LINK" -> $SOURCE_DIR"
+  done
 done
