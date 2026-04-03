2f562e429e635d4c67141fe4b3b916e604ef652d
Eduardo Chiaro <e.chiaro@gmail.com>
2026-04-03 21:20:35 +0200

    Add Tracker Alliance logos; centralize splash resources

--- standard-edition/src/c/modules/splash_logo_module.c
+++ standard-edition/src/c/modules/splash_logo_module.c
@@ -1,16 +1,5 @@
 #include "splash_logo_module.h"
-
-#define SPLASH_LOGO_COUNT 7
-
-static const uint32_t s_logo_resources[SPLASH_LOGO_COUNT] = {
-  RESOURCE_ID_CONSTELLATION_BW_LOGO_IMAGE,    // 1
-  RESOURCE_ID_CONSTELLATION_COLOR_LOGO_IMAGE, // 2
-  RESOURCE_ID_HOUSEVARUUN_LOGO_IMAGE,         // 3
-  RESOURCE_ID_FREESTAR_LOGO_IMAGE,            // 4
-  RESOURCE_ID_SYSDEF_LOGO_IMAGE,              // 5
-  RESOURCE_ID_CRIMSON_LOGO_IMAGE,             // 6
-  RESOURCE_ID_MOON_BACKGROUND_IMAGE,          // 7
-};
+#include "../utilities/logos.h"
 
 static BitmapLayer *s_splash_logo_layer;
 static GBitmap *s_splash_bitmap;
